eb741597b28c6d2fd99f33c083f9e7b1ec799366
ANAM <anamshaikh0515@gmail.com>
2023-11-18 11:07:01 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(name="census-income",
+      version="0.0.1",
+      author="anam",
+      author_email="anamshaikh0515@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+)
